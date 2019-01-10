f19ab8c8c7312a4efee08db7bf2777f0456ad659
sujeong <ssostory@gmail.com>
2019-01-11 00:05:13 +0900

    project01 작업중

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(function(){
-  var menu = $('#header_wrap');  
 
+  // 상단메뉴 고정
+  var menu = $('#header_wrap');  
   $(window).each(function(){
     $(window).scroll(function(){
       var winTop = $(window).scrollTop();
@@ -11,4 +12,51 @@ $(function(){
       }
     });
   });  
+
+  // SUB페이지 보기 아코디언  !!!!!!!수정이 필요함
+  $('activePanel').animate({duration:600,queue:false,easing:'easeOutCubic'});
+  var viewsubName = $('.viewsub > ul > li');
+  viewsubName.mouseover(function(){
+    viewsubName.removeClass('activePanel');
+    $(this).addClass('activePanel');
+    
+  });
+
+  //SUB페이지 보기 아코디언
+  var nav = $('.viewsub li');
+  nav.hover(
+    function(){
+      close();
+      var tg = $(this);
+      var menuImage = tg.find('.subImage');
+      //menuImage = 마우스 올렸을 때 보여지는 이미지
+      var imageWidth = menuImage.find('img').innerWidth();
+      menuImage.animate({width:imageWidth},{duration:600,queue:false,easing:'easeOutCubic'});
+    },
+    function(){
+      var tg = $(this);
+      var menuImage = tg.find('.subImage');
+      var imageWidth = 0;
+      menuImage.animate({width:imageWidth},{duration:600,queue:false,easing:'easeOutCubic'});
+      open();	
+    }
+  );
+  open();	
+  function open(){		
+      var tg = $('.viewsub li').filter('.openli');
+      var menuImage = tg.find('.subImage');
+      var imageWidth = menuImage.find('img').innerWidth();
+      menuImage.animate({width:imageWidth},{duration:600,queue:false,easing:'easeOutCubic'});		
+  }
+  
+  function close(){
+      var tg = $('.viewsub li').filter('.openli');
+      var menuImage = tg.find('.subImage');
+      var imageWidth = 0;
+      menuImage.animate({width:imageWidth},{duration:600,queue:false,easing:'easeOutCubic'});
+  }	
+
+
+
+
 });
